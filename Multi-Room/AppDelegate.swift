fd9f6cd87136523c1e91ad543fd7ce16bb8888fd
Patrick Busch <git.p@trickbusch.de>
2018-05-01 15:49:22 +0200

    initial commit

--- Multi-Room/AppDelegate.swift
+++ Multi-Room/AppDelegate.swift
@@ -10,10 +10,15 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("Audio"))
+            button.action = #selector(doSomething(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -21,5 +26,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    @objc func doSomething(_ sender: Any?) {
+        print("Button pressed")
+    }
 }
 
