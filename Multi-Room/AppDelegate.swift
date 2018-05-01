a4ee669f51f545ddc2dfac5ae9ab2b0ca50d46c6
Patrick Busch <git.p@trickbusch.de>
2018-05-01 16:10:29 +0200

    popover

--- Multi-Room/AppDelegate.swift
+++ Multi-Room/AppDelegate.swift
@@ -11,16 +11,19 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("Audio"))
-//            button.action = #selector(doSomething(_:))
+            button.action = #selector(togglePopover(_:))
         }
+
+        popover.contentViewController = TestViewController.freshController()
         
-        self.constructMenu()
+//        self.constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -28,6 +31,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    
+    // MARK: Menu Stuff
     @objc func doSomething(_ sender: Any?) {
         print("Button pressed")
     }
@@ -35,11 +40,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func constructMenu() {
         let menu = NSMenu()
         
-        menu.addItem(NSMenuItem(title: NSLocalizedString("Do something", comment: ""), action: #selector(AppDelegate.doSomething(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem(title: NSLocalizedString("Do something", comment: ""), action: #selector(AppDelegate.doSomething(_:)), keyEquivalent: ""))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: NSLocalizedString("Quit application", comment: ""), action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
         statusItem.menu = menu
+    }
+    
+    // MARK: Popover
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
     }
 }
 
