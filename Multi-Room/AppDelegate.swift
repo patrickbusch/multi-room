147f740cea52c595b6805a2310b6c6268cf515b8
Patrick Busch <git.p@trickbusch.de>
2018-05-01 16:39:42 +0200

    menu only on right click

--- Multi-Room/AppDelegate.swift
+++ Multi-Room/AppDelegate.swift
@@ -13,18 +13,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     let popover = NSPopover()
     var eventMonitor: EventMonitor?
+    var menu: NSMenu?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("Audio"))
-            button.action = #selector(togglePopover(_:))
+            button.action = #selector(buttonClicked(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
 
         popover.contentViewController = TestViewController.freshController()
         
 //        self.constructMenu()
+        
+        self.menu = self.constructMenu()
         
         self.eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
             if let strongSelf = self, strongSelf.popover.isShown {
@@ -38,20 +42,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    // MARK: Click Handling
+    @objc func buttonClicked(_ sender: Any?) {
+        let event = NSApp.currentEvent!
+        
+        if event.type == NSEvent.EventType.rightMouseUp {
+//            constructMenu()
+            self.statusItem.popUpMenu(self.menu ?? NSMenu())
+        } else if event.type == NSEvent.EventType.leftMouseUp {
+            togglePopover(sender)
+        }
+    }
     
     // MARK: Menu Stuff
     @objc func doSomething(_ sender: Any?) {
         print("Button pressed")
     }
     
-    func constructMenu() {
+    func constructMenu() -> NSMenu {
         let menu = NSMenu()
         
         menu.addItem(NSMenuItem(title: NSLocalizedString("Do something", comment: ""), action: #selector(AppDelegate.doSomething(_:)), keyEquivalent: ""))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: NSLocalizedString("Quit application", comment: ""), action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
-        statusItem.menu = menu
+        return menu
+//        statusItem.menu = menu
     }
     
     // MARK: Popover
