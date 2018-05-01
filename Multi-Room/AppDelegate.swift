a632eb23289d387d818cefa033fa5bc37259448f
Patrick Busch <git.p@trickbusch.de>
2018-05-01 10:15:50 +0200

    Initial Commit

---
+++ Multi-Room/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Multi-Room
+//
+//  Created by Patrick Busch on 01.05.18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Multi-Room/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Multi-Room
+//
+//  Created by Patrick Busch on 01.05.18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
