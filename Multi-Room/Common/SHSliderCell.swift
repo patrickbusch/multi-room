26df00a90f3002fc9093babae2655acad46fbd67
Patrick Busch <git.p@trickbusch.de>
2018-07-07 13:43:54 +0200

    nicer sliders

--- Multi-Room/Common/SHSliderCell.swift
+++ Multi-Room/Common/SHSliderCell.swift
@@ -28,19 +28,17 @@ class SHSliderCell: NSSliderCell {
         
         let knobRect = self.knobRect(flipped: false)
         
-//        let tickStrength: CGFloat = 2
-        let longerSide: CGFloat = 8
-        let shorterSide: CGFloat = 1
-        
         if (slider.isVertical) {
             
+            let width: CGFloat = slider.bounds.width / 4
+            let height: CGFloat = 1
             let horizontalInset: CGFloat = 1
             let verticalInset: CGFloat = knobRect.height / 2
             let drawRect = slider.bounds.insetBy(dx: horizontalInset, dy: verticalInset) //squish in slightly
             let step = drawRect.height/CGFloat(numberOfTickMarks - 1) //classic fenceposts problem
             var mark = CGFloat(0)
             for _ in 0..<numberOfTickMarks {
-                let blob = NSBezierPath(roundedRect: NSRect(x: horizontalInset , y: mark + verticalInset, width: longerSide, height: shorterSide), xRadius: 0, yRadius: 0)
+                let blob = NSBezierPath(roundedRect: NSRect(x: horizontalInset , y: mark + verticalInset, width: width, height: height), xRadius: 0, yRadius: 0)
                 self.color.set()
                 blob.fill()
                 self.color.set()
@@ -49,13 +47,15 @@ class SHSliderCell: NSSliderCell {
             }
         } else {
             
+            let width: CGFloat = 1
+            let height: CGFloat = slider.bounds.height / 4
             let horizontalInset: CGFloat = knobRect.width / 2
             let verticalInset: CGFloat = 1
             let drawRect = slider.bounds.insetBy(dx: horizontalInset, dy: verticalInset) //squish in slightly
             let step = drawRect.width/CGFloat(numberOfTickMarks - 1) //classic fenceposts problem
             var mark = CGFloat(0)
             for _ in 0..<numberOfTickMarks {
-                let blob = NSBezierPath(roundedRect: NSRect(x: mark + horizontalInset , y:verticalInset, width: shorterSide, height: longerSide), xRadius: 0, yRadius: 0)
+                let blob = NSBezierPath(roundedRect: NSRect(x: mark + horizontalInset , y:verticalInset, width: width, height: height), xRadius: 0, yRadius: 0)
                 self.color.set()
                 blob.fill()
                 self.color.set()
